a79d74f4f56f8d492721a87feee55fd47e4c3d86
Christian <christian.pinillah@sansano.usm.cl>
2022-06-29 12:54:03 -0400

    CREATE: Se agrego scribe

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use App\Models\Prueba;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        return view('index');
+    }
+
 }
